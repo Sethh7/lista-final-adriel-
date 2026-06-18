08482478eb262b835c50c9394b1022e337368944
Adriel <adrielrodrigues156@gmail.com>
2026-06-17 23:49:58 -0300

    Implementação: Função para verificar se tem ciclos.

--- challenges/has_cycle.py
+++ challenges/has_cycle.py
@@ -1,2 +1,23 @@
 def has_cycle(graph: dict[str, list[str]]) -> bool:
-    raise NotImplementedError
+    no_visitado = set()
+    no_caminho = set()
+
+    def busca_em_profundidade(busca):
+        no_visitado.add(busca)
+        no_caminho.add(busca)
+
+        for vizinho in graph.get(busca,[]):
+            if vizinho not in no_visitado:
+                if busca_em_profundidade(vizinho):
+                    return True
+            elif vizinho in no_caminho:
+                return True
+
+        no_caminho.remove(busca)
+        return False
+    
+    for busca in graph:
+        if busca not in no_visitado:
+            if busca_em_profundidade(busca):
+                return True
+    return False
