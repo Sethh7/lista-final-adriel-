4adfd0ff1c45375261ea0af6fdd032d1d79f36f3
Adriel <adrielrodrigues156@gmail.com>
2026-06-17 23:26:32 -0300

    Implemtenção: Função agrupando Anagramas.

--- challenges/group_anagrams.py
+++ challenges/group_anagrams.py
@@ -1,2 +1,9 @@
 def group_anagrams(words: list[str]) -> list[list[str]]:
-    raise NotImplementedError
+    organizador = {}
+    for palavra in words:
+        transformacoa_palavra ="".join(sorted(palavra))
+        if transformacoa_palavra not in organizador:
+            organizador[transformacoa_palavra] = []
+                        
+        organizador[transformacoa_palavra].append(palavra)
+    return list(organizador.values())
