cfedf237e00b011f1d3289092360c5a6a9ae789a
Adriel <adrielrodrigues156@gmail.com>
2026-06-17 23:50:40 -0300

    Implementação: Função Menor Ancestral Comum.

--- challenges/lowest_common_ancestor.py
+++ challenges/lowest_common_ancestor.py
@@ -6,4 +6,21 @@ def lowest_common_ancestor(
     value1: int,
     value2: int,
 ) -> int:
-    raise NotImplementedError
+    if root is None:
+        return -1
+
+    no_atual = root
+
+    while no_atual is not None:
+        valor_atual = no_atual.get_value()
+
+        if value1 < valor_atual and value2 < valor_atual:
+            no_atual = no_atual.get_left_child()
+            
+        elif value1 > valor_atual and value2 > valor_atual:
+            no_atual = no_atual.get_right_child()
+            
+        else:
+            return valor_atual
+
+    return -1
